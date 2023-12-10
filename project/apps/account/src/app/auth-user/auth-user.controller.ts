75d9382214c6a9ff194304817b80fea78b07b0c5
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2023-12-10 20:59:20 +0300

    feat: Добавит обработчик маршрута /register

--- project/apps/account/src/app/auth-user/auth-user.controller.ts
+++ project/apps/account/src/app/auth-user/auth-user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthUserService } from './auth-user.service';
+import { UserRdo } from './rdo/user.rdo';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fillDto } from '@project/shared/helpers';
 
 @Controller('auth-user')
 export class AuthUserController {
   constructor(
     private readonly authService: AuthUserService
   ) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillDto(UserRdo, newUser.toPOJO());
+  }
 }
