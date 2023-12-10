cbfca07f717a59e8a8c1c1274aa7fe1be755735d
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2023-12-10 19:46:59 +0300

    feat: добавит DTO: CreateUserDto и LoginUserDto

---
+++ project/apps/account/src/app/auth-user/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  public email: string;
+  public firstname: string;
+  public lastname: string;
+  public avatar: string;
+  public registeredAt: string;
+  public password: string;
+}
