fb001016f9ac68bd3825cd60f22a32c508d9f8e4
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2023-12-10 19:29:34 +0300

    feat: добавит импорт модуля BlogUserModule

--- project/apps/account/src/app/auth-user/auth-user.module.ts
+++ project/apps/account/src/app/auth-user/auth-user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthUserService } from './auth-user.service';
 import { AuthUserController } from './auth-user.controller';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 
 @Module({
+  imports: [BlogUserModule],
   providers: [AuthUserService],
   controllers: [AuthUserController],
 })
