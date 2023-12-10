4fbae2e26778c7ad85316d9cb40b037f354708d7
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2023-12-10 16:43:52 +0300

    feat: создаёт директорий-заготовку для сервиса и контроллера сервиса auth-user

--- project/apps/account/src/app/auth-user/auth-user.module.ts
+++ project/apps/account/src/app/auth-user/auth-user.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthUserService } from './auth-user.service';
+import { AuthUserController } from './auth-user.controller';
 
-@Module({})
+@Module({
+  providers: [AuthUserService],
+  controllers: [AuthUserController],
+})
 export class AuthUserModule {}
